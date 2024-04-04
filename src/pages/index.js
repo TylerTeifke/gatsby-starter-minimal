fbac5c250baedbc0fcf0257c7f5ae7fe7b822903
TylerTeifke <114880372+TylerTeifke@users.noreply.github.com>
2024-04-04 13:16:43 -0700

    Added hello world message

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = () => {
       <h1>Welcome to my Gatsby site!</h1>
       <Link to="/about">About</Link>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>Hello World</p>
     </main>
   )
 }
