4c1b0f9c87dc5512ce0a78946808985f27955757
TylerTeifke <114880372+TylerTeifke@users.noreply.github.com>
2024-04-04 13:04:22 -0700

    Finished Gatsby tutorial part 2

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+// Step 1: Import React
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <Link to="/">Back to Home</Link>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p>I like video games. My favorite game is Hollow Knight</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+
+// Step 3: Export your component
+export default AboutPage
